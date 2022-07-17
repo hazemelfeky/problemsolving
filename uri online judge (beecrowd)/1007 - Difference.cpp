cf8853621b957ce7d84fdb53af29825f7cd3e2e4
hazemelfeky <71050085+hazemelfeky@users.noreply.github.com>
2022-07-17 19:11:40 +0200

    Create 1007 - Difference.cpp

---
+++ uri online judge (beecrowd)/1007 - Difference.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+  int a, b, c, d, res;
+  cin>>a>>b>>c>>d;
+  res = a*b - c*d;
+  cout<<"DIFERENCA = "<<res<<endl;
+  return 0;
+}
